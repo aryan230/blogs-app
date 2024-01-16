e6b9493d2281472339e9f518e35e1f5c55da369b
aryan230 <aryan23062001@gmail.com>
2024-01-16 05:30:22 +0530

    final-fix

--- app/page.js
+++ app/page.js
@@ -2,9 +2,17 @@
 
 import Blogs from "@/components/Blogs";
 import Header from "@/components/Header";
+import { useBearStore } from "@/store/zustand";
 import Image from "next/image";
+import { useEffect } from "react";
 
 export default function Home() {
+  const { setUser } = useBearStore();
+  useEffect(() => {
+    if (localStorage.getItem("userInfo")) {
+      setUser(JSON.parse(localStorage.getItem("userInfo")).user);
+    }
+  }, []);
   return (
     <main className="">
       <Header />
--- components/Blogs.jsx
+++ components/Blogs.jsx
@@ -23,7 +23,7 @@ function Blogs() {
         setBlogs(data);
       });
     }
-  }, []);
+  }, [user]);
 
   console.log(blogs);
   return (
