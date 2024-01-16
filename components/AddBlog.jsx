a9d484c4b8646ce5c280cf2f0c3ee4e426d4cc4f
aryan230 <aryan23062001@gmail.com>
2024-01-16 05:45:56 +0530

    removed-local

--- components/AddBlog.jsx
+++ components/AddBlog.jsx
@@ -1,7 +1,5 @@
 "use client";
 
-/* This example requires Tailwind CSS v2.0+ */
-
 import { Fragment, useRef, useState } from "react";
 import { Dialog, Transition } from "@headlessui/react";
 import { ExclamationIcon } from "@heroicons/react/outline";
